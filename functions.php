40af3e753e2f9a7e36243acb0b3d7a8a8d5b4af2
theimaginaire <seb.dean@theimaginaire.com>
2016-04-27 16:35:07 +0100

    Shortcodes file added

--- functions.php
+++ functions.php
@@ -15,6 +15,7 @@ $sage_includes = [
   'lib/setup.php',     // Theme setup
   'lib/titles.php',    // Page titles
   'lib/wrapper.php',   // Theme wrapper class
+  'lib/shortcodes.php',   // Theme wrapper class
   'lib/customizer.php' // Theme customizer
 ];
 
