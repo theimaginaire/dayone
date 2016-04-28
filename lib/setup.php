fe0acb0d922fa7665bcd6cb34434d5d8c3e0b77d
theimaginaire <seb.dean@theimaginaire.com>
2016-04-28 16:03:55 +0100

    added woocomm pages

--- lib/setup.php
+++ lib/setup.php
@@ -15,7 +15,7 @@ function setup() {
   add_theme_support('soil-nice-search');
   add_theme_support('soil-jquery-cdn');
   add_theme_support('soil-relative-urls');
-
+  add_theme_support('woocommerce');
   // Make theme available for translation
   // Community translations can be found at https://github.com/roots/sage-translations
   load_theme_textdomain('sage', get_template_directory() . '/lang');
