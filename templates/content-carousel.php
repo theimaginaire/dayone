d7f814b01e489dcc8d5f4c39c9aca4a151ff3480
theimaginaire <seb.dean@theimaginaire.com>
2017-01-08 14:40:58 +0000

    Dynamic Carousel

--- templates/content-carousel.php
+++ templates/content-carousel.php
@@ -1,11 +1,37 @@
+<?php
+
+// check if the repeater field has rows of data
+if( have_rows('carousel') ):
+?>
 <section class="owl-carousel owl-theme">
-	<div class="item">
-	spaghetti
-	</div>
-	<div class="item">
-blamonge
-	</div>
-	<div class="item">
-potato
+<?php 
+ 	// loop through the rows of data
+    while ( have_rows('carousel') ) : the_row();
+
+    // Individual carousel slide
+?>
+<div class="item" style="background-image:url(<?php the_sub_field('background_image'); ?>);">
+<div class="item-content">
+	<?php the_sub_field('slide_content'); ?>
+</div>
+</div>
+<?php
+    
+    endwhile;
+?>
+</section>
+<?php
+elseif(get_field('hero_content')) :
+
+?>
+<section class="hero" <?php if(get_field('hero_image')): ?> style="background-image:url(<?php the_field('hero_image'); ?>);" <?php endif; ?>>
+	<div class="hero-content">
+	<?php the_field('hero_content'); ?>
 	</div>
 </section>
+<?php 
+else: 
+	get_template_part('templates/page', 'header');
+endif;
+
+?>
